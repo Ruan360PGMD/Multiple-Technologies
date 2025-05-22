f2c81e4f9b2abcedc951fb8f94adb012f857fcff
PGMD <adayltomruja@gmail.com>
2025-05-22 05:35:12 -0300

    Mod v2 test

---
+++ src/code/Load.java
@@ -0,0 +1,7 @@
+package code;
+
+import arc.*;
+import block.pers;
+
+new block1 = FontSinal()
+new block2 = BlocoControleArmazenamento("test1")
---
+++ src/code/block.java
@@ -0,0 +1,79 @@
+package blocks.pers;
+
+import mindustry.world.Block;
+import mindustry.world.BlockGroup;
+import mindustry.world.Tile;
+import mindustry.world.blocks.Building;
+import mindustry.type.Item;
+import mindustry.content.Items;
+import mindustry.ctype.Content;
+import mindustry.ctype.ContentList;
+import mindustry.Vars;
+import mindustry.gen.Call;
+
+public class FontSinal extends Block{
+
+
+}
+
+
+public class BlocoControleArmazenamento extends Block {
+
+    public BlocoControleArmazenamento(String name) {
+        super(name);
+
+        this.update = true;
+        this.hasItems = true;
+        this.group = BlockGroup.transportation;
+        this.solid = true;
+        this.size = 1;
+        this.itemCapacity = 100; // capacidade interna de itens
+    }
+
+    public class BlocoControleBuild extends Building {
+        int timer = 0;
+
+        @Override
+        public void updateTile() {
+            if (timer++ % 20 != 0) return; // a cada 20 ticks
+
+            for (int i = 0; i < 4; i++) {
+                Building vizinho = nearby(i);
+                if (vizinho == null || vizinho.items == null) continue;
+
+                for (Item item : content.items()) {
+                    if (vizinho.items.has(item) && this.acceptItem(this, item)) {
+                        vizinho.items.remove(item, 1);
+                        this.items.add(item, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public boolean outputsItems() {
+        return true;
+    }
+
+    @Override
+    public boolean hasLiquids() {
+        return false;
+    }
+
+    @Override
+    public void load() {
+        super.load();
+    }
+
+    @Override
+    public Building createBuilding() {
+        return new BlocoControleBuild();
+    }
+}
